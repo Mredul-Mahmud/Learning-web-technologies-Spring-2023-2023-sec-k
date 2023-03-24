3bf43a602af6f216c0d3edac331976f181d99c1f
Mredul-Mahmud <123942193+Mredul-Mahmud@users.noreply.github.com>
2023-03-24 22:03:23 +0600

    Add files via upload

--- Mid Project/Pa_logincheck.php
+++ Mid Project/Pa_logincheck.php
@@ -1,53 +1,44 @@
-<?php 
-    session_start();
-    
-    if(isset($_REQUEST['submit']))
-    {
+
+
+<?php
+session_start();
+if(isset($_REQUEST['submit']))
+{
+    $username= $_REQUEST['username'];
+    $password= $_REQUEST['password'];
+
+    $flag= false;
+    if( $username=='' || $password == ''){
+        echo " <h3 align='center'>Username or Password can not be empty </h3>";
         
-        $username = $_REQUEST['username'];
-        $password = $_REQUEST['password'];
-        $flag = false;
-
-        if($username == "" && $password == "")
-        {
-            echo "Please enter username/password";
-        }
-        else
-        {
-
-            $file = fopen('user.txt', 'r');
-            while(!feof($file))
-            {
-                $data = fgets($file);
-                $user = explode('|', $data);
-                if($username == trim($user[0]) && $password == trim($user[3]))
-                {
-                    $flag = true;
-                }
-            }
-            if($flag & $user[0]= $username)
-            {
-                if($user[3]= $password)
-                {
-                    $_SESSION['username'] = $username;
-                    $_SESSION['password'] = $password;
-               
-                fclose($file);
-                
-
-                setcookie('flag', 'true', time()+300, '/');
-                header('location: home.php');
-                }
-                
-            }
-            else
-            {
-                header('location: userlogin.php?msg=error');
-            }
-        }
     }
-    else
-    {
-        echo "Something went wrong";
+    else {
+$file = fopen('user.txt','r');
+while(!feof($file)){
+    $data= fgets($file);
+    $user= explode('|', $data);
+    if($username==trim($user[0]) && $password == trim($user[3])){
+            $flag= true; }
+            if($flag){
+             $_SESSION['username']= $username;
+             $email= $user[1];
+             fclose($file);
+             $_SESSION['email']=$email;
+            setcookie('consumer_logged_flag',true,time()+400,'/');
+            if ($_REQUEST['remember']){
+                setcookie('consumer_logged_flag',true,time()+10000000000,'/'); 
+            }
+            header("location:home.php");}
+            else{
+                    header('location: userlogin.php?msg=error');
+                    
+            }
+}
     }
+
+}
+else
+{
+    echo "<h2 align='center'>Invalid User Request</h2>";
+}
 ?>
